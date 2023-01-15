c59b69de1ad210382176391bbddf8eb9ebe92f5c
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-15 18:57:20 +0100

    add bootstrap style and module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxBootstrapModule } from './modules/ngx-bootstrap.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NgxBootstrapModule, // Ajoutez l’exportation de NgxBootstrap
+  ],
   exports: [CommonModule],
 })
 export class SharedModule {}
