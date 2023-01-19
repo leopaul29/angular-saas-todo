81066dcae359fe7164cd94222ef27244f6ccd990
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-19 21:15:43 +0100

    create and add planing compo

---
+++ src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'al-planning-workday-item',
+  templateUrl: './planning-workday-item.component.html',
+  styles: [
+  ]
+})
+export class PlanningWorkdayItemComponent {
+
+}
---
+++ src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'al-planning-workday-list',
+  templateUrl: './planning-workday-list.component.html',
+  styles: [
+  ]
+})
+export class PlanningWorkdayListComponent {
+
+}
