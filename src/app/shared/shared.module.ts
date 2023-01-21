15ee2ad26a9918140522ce3699521861cc6eeac6
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-21 14:19:15 +0100

    add sidenavbar for connected users

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxBootstrapModule } from './modules/ngx-bootstrap.module';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidenavComponent
+  ],
   imports: [CommonModule, NgxBootstrapModule],
   exports: [
     CommonModule,
-    NgxBootstrapModule, // Ajoutez l’exportation de NgxBootstrap
+    NgxBootstrapModule,
+    SidenavComponent, // Ajoutez l’exportation de NgxBootstrap
   ],
 })
 export class SharedModule {}
