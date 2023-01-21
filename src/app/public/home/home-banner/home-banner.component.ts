761672a135f7317797244807d5178d5b8448c15a
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-21 14:56:27 +0100

    add home redirection button

--- src/app/public/home/home-banner/home-banner.component.ts
+++ src/app/public/home/home-banner/home-banner.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'al-home-banner',
   templateUrl: './home-banner.component.html',
   styleUrls: ['./home-banner.component.css'],
 })
-export class HomeBannerComponent {}
+export class HomeBannerComponent {
+  constructor(private router: Router) {}
+
+  navigateToDashboard(): void {
+    this.router.navigate(['app/dashboard']);
+  }
+}
