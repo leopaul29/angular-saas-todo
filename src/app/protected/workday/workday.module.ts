e3408c27c83d0d77ff32f813c2ddd6323201f162
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-21 23:06:36 +0100

    add workday form date compo

--- src/app/protected/workday/workday.module.ts
+++ src/app/protected/workday/workday.module.ts
@@ -3,9 +3,14 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { WorkdayComponent } from './workday/workday.component';
 import { WorkdayRoutingModule } from './workday-routing.module';
 import { WorkdayFormComponent } from './workday-form/workday-form.component';
+import { WorkdayFormDateComponent } from './workday-form-date/workday-form-date.component';
 
 @NgModule({
-  declarations: [WorkdayComponent, WorkdayFormComponent],
+  declarations: [
+    WorkdayComponent,
+    WorkdayFormComponent,
+    WorkdayFormDateComponent,
+  ],
   imports: [SharedModule, WorkdayRoutingModule],
 })
 export class WorkdayModule {}
