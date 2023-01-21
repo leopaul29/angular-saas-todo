57bf20ac8bab4d8fc0483e4518ced09fe1cf678b
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-21 23:04:25 +0100

    add workday dumb form component

--- src/app/protected/workday/workday.module.ts
+++ src/app/protected/workday/workday.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { WorkdayComponent } from './workday/workday.component';
 import { WorkdayRoutingModule } from './workday-routing.module';
+import { WorkdayFormComponent } from './workday-form/workday-form.component';
 
 @NgModule({
-  declarations: [
-    WorkdayComponent
-  ],
+  declarations: [WorkdayComponent, WorkdayFormComponent],
   imports: [SharedModule, WorkdayRoutingModule],
 })
 export class WorkdayModule {}
