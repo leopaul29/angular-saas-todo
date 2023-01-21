0ab0f8afaaedd6ac88d048e86f1a30a6520f975d
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-21 23:07:05 +0100

    add workday form task item compo

--- src/app/protected/workday/workday.module.ts
+++ src/app/protected/workday/workday.module.ts
@@ -4,12 +4,14 @@ import { WorkdayComponent } from './workday/workday.component';
 import { WorkdayRoutingModule } from './workday-routing.module';
 import { WorkdayFormComponent } from './workday-form/workday-form.component';
 import { WorkdayFormDateComponent } from './workday-form-date/workday-form-date.component';
+import { WorkdayFormTasksItemComponent } from './workday-form-tasks-item/workday-form-tasks-item.component';
 
 @NgModule({
   declarations: [
     WorkdayComponent,
     WorkdayFormComponent,
     WorkdayFormDateComponent,
+    WorkdayFormTasksItemComponent,
   ],
   imports: [SharedModule, WorkdayRoutingModule],
 })
