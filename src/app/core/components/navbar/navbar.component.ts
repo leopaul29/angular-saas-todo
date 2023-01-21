2731afa189a412b78cd55135a1c0a3cd5e43a893
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-21 14:14:11 +0100

    update navbar

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'al-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  homePath: string = 'home';
+  loginPath: string = 'login';
+  registerPath: string = 'register';
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  public isActive(page: string): boolean {
+    return this.router.isActive(page, true);
+  }
+
+  public navigate(page: string): void {
+    this.router.navigate([page]);
+  }
 }
