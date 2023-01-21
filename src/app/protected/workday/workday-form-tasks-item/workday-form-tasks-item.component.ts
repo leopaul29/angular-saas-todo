6d824b768857bf89350a9387fc5dcb5796bea5a8
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-22 00:53:43 +0100

    remove task form action

--- src/app/protected/workday/workday-form-tasks-item/workday-form-tasks-item.component.ts
+++ src/app/protected/workday/workday-form-tasks-item/workday-form-tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'al-workday-form-tasks-item',
@@ -7,6 +7,15 @@ import { FormGroup } from '@angular/forms';
 })
 export class WorkdayFormTasksItemComponent implements OnInit {
   @Input() task: FormGroup;
+  @Input() index: number;
+  @Input() isFirst: boolean;
+  @Input() isLast: boolean;
+
+  @Output() removedTask = new EventEmitter<number>();
 
   ngOnInit(): void {}
+
+  removeTask(index: number) {
+    this.removedTask.emit(index);
+  }
 }
