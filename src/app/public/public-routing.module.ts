f13b1f35ab76429702216e4b2daa2325733bbb8d
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-21 13:16:54 +0100

    update public routing

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './login/login/login.component';
+import { RegisterComponent } from './register/register/register.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
