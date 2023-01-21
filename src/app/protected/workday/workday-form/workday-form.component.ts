3c3651a3d62bed7cd4a2b78ed8be27ea8287e17c
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-21 23:17:46 +0100

    add workday main form and validation

--- src/app/protected/workday/workday-form/workday-form.component.ts
+++ src/app/protected/workday/workday-form/workday-form.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'al-workday-form',
   templateUrl: './workday-form.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class WorkdayFormComponent {
+export class WorkdayFormComponent implements OnInit {
+  workdayForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.workdayForm = this.createWorkdayForm();
+  }
+
+  get dueDate() {
+    return this.workdayForm.get('dueDate') as FormControl;
+  }
+  get notes() {
+    return this.workdayForm.get('notes') as FormControl;
+  }
+  get tasks() {
+    return this.workdayForm.get('tasks') as FormArray;
+  }
+
+  createWorkdayForm(): FormGroup {
+    const workdayForm: FormGroup = this.fb.group({
+      dueDate: '',
+      tasks: this.fb.array([]),
+      notes: '',
+    });
+
+    return workdayForm;
+  }
+
+  submit(): void {
+    console.info(this.workdayForm.value);
+  }
 }
