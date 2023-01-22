7a7b0563d72028320d6732ab58387f4baf6ff8ed
Leo-paul <leopaulmartin@hotmail.fr>
2023-01-22 12:20:21 +0100

    remove datepicker custome validator logs

--- src/app/shared/validators/is-date.validator.ts
+++ src/app/shared/validators/is-date.validator.ts
@@ -1,26 +1,8 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export function IsDateValidator(control: AbstractControl): ValidatorFn {
-  console.log('control.value', control.value);
-  console.log(
-    "new Date(control.value).toString() !== 'Invalid Date'",
-    new Date(control.value).toString() !== 'Invalid Date'
-  );
-  console.log(
-    'isNaN(new Date(control.value).getTime())',
+  return new Date(control.value).toString() !== 'Invalid Date' &&
     isNaN(new Date(control.value).getTime())
-  );
-  console.log(
-    'condition date',
-    new Date(control.value).toString() !== 'Invalid Date' &&
-      !isNaN(new Date(control.value).getTime())
-  );
-  const a =
-    new Date(control.value).toString() !== 'Invalid Date' &&
-    isNaN(new Date(control.value).getTime())
-      ? { isDate: true }
-      : (null as any);
-  console.log('isDate', a);
-
-  return a;
+    ? { isDate: true }
+    : (null as any);
 }
